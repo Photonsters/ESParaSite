efce536e2ef8fc806a859853fb186d65c1cca220
aeakin1 <aeakin1@gmail.com>
2019-05-19 19:32:45 -0400

    Update ESParaSite_Core.cpp

--- src/ESParaSite_Core.cpp
+++ src/ESParaSite_Core.cpp
@@ -33,9 +33,6 @@
 //const char* wifi_ssid     = "yourwifinetwork";
 //const char* wifi_password = "yourwifipassword";
 
-const char *wifi_ssid = "mxbnet";
-const char *wifi_password = "Presto1234!";
-
 //+++ Advanced Settings +++
 // For precise altitude measurements please put in the current pressure corrected for the sea level
 // Otherwise leave the standard pressure as default (1013.25 hPa);
