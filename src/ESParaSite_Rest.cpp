2bb5a9cc41df17945283bebc974c9c8d34528d81
aeakin1 <aeakin1@gmail.com>
2019-05-19 19:30:23 -0400

    Massive updates
    More and more updates!

--- src/ESParaSite_Rest.cpp
+++ src/ESParaSite_Rest.cpp
@@ -14,14 +14,24 @@
 
 #include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
-#include "ESParaSite_Settings.hxx"
 #include "ESParaSite_Core.h"
 #include "ESParaSite_Rest.h"
+
+
+//+++ User Settings +++
+
+//Enter the port on which you want the HTTP server to run (Default is 80).
+//You will access the server at http://<ipaddress>:<port>
+#define HTTP_REST_PORT 80
+
+//*** DO NOT MODIFY ANYTHING BELOW THIS LINE ***
+
+ESP8266WebServer http_rest_server(HTTP_REST_PORT);
 
 void config_rest_server_routing()
 {
   http_rest_server.on("/", HTTP_GET, []() {
-    http_rest_server.send(200, "text/html",
+  http_rest_server.send(200, "text/html",
                           "Welcome to the ESParasite REST Web Server");
   });
   http_rest_server.on("/printchamber", HTTP_GET, get_chamber);
@@ -30,6 +40,17 @@ void config_rest_server_routing()
   http_rest_server.on("/enclosure", HTTP_GET, get_enclosure);
   // http_rest_server.on("/enclosure", HTTP_POST, post_enclosure); //Not yet implemented
   // http_rest_server.on("/enclosure", HTTP_PUT, post_enclosure);  //Not yet implemented
+  Serial.print("HTTP REST config complete!");
+}
+
+void do_client()
+{
+  http_rest_server.handleClient();
+}
+
+void start_http_server()
+{
+  http_rest_server.begin();
 }
 
 void get_chamber()
@@ -42,10 +63,10 @@ void get_chamber()
   StaticJsonDocument<256> doc;
 
   doc["class"] = "chamber";
-  doc["timestamp"] = timestamp;
-  doc["seconds_t"] = chamber_resource.dht_temp_c;
-  doc["seconds_s"] = chamber_resource.dht_humidity;
-  doc["seconds_l"] = chamber_resource.dht_dewpoint;
+  doc["timestamp"] = rtc_timestamp;
+  doc["chmb_temp_c"] = chamber_resource.dht_temp_c;
+  doc["chmb_humidity"] = chamber_resource.dht_humidity;
+  doc["chmb_dewpoint"] = chamber_resource.dht_dewpoint;
 
   serializeJson(doc, Serial);
   Serial.println();
@@ -69,7 +90,7 @@ void get_optics()
   StaticJsonDocument<256> doc;
 
   doc["class"] = "optics";
-  doc["timestamp"] = timestamp;
+  doc["timestamp"] = rtc_timestamp;
   doc["uvindex"] = optics_resource.si_uvindex;
   doc["visible"] = optics_resource.si_visible;
   doc["infrared"] = optics_resource.si_infrared;
@@ -97,7 +118,7 @@ void get_ambient()
   StaticJsonDocument<256> doc;
 
   doc["class"] = "ambient";
-  doc["timestamp"] = timestamp;
+  doc["timestamp"] = rtc_timestamp;
   doc["amb_temp_c"] = ambient_resource.bme_temp_c;
   doc["amb_humidity"] = ambient_resource.bme_humidity;
   doc["amb_pressure"] = ambient_resource.bme_barometer;
@@ -124,11 +145,12 @@ void get_enclosure()
   StaticJsonDocument<256> doc;
 
   doc["class"] = "enclosure";
-  doc["timestamp"] = timestamp;
+  doc["timestamp"] = rtc_timestamp;
   doc["case_temp"] = enclosure_resource.case_temp;
-  doc["seconds_t"] = enclosure_resource.total_sec;  //Placeholder - Not yet Implemented
-  doc["seconds_s"] = enclosure_resource.screen_sec; //Placeholder - Not yet Implemented
-  doc["seconds_l"] = enclosure_resource.led_sec;    //Placeholder - Not yet Implemented
+  doc["sec_tot"] = enclosure_resource.total_sec;  //Placeholder - Not yet Implemented
+  doc["sec_scr"] = enclosure_resource.screen_sec; //Placeholder - Not yet Implemented
+  doc["sec_led"] = enclosure_resource.led_sec;    //Placeholder - Not yet Implemented
+  doc["sec_fep"] = enclosure_resource.fep_sec;    //Placeholder - Not yet Implemented
 
   serializeJson(doc, Serial);
   Serial.println();
